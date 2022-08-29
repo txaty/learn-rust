3cf8825ece18eef7c5f673ba57efd369dac1b3f1
txaty <xtianae@connect.ust.hk>
2022-08-29 11:21:14 +0800

    1. Hello world

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# learn-rust
+# Learn Rust
+
+My Rust programming language learning journey.
+
+With online book: [The Rust Programming Language](https://doc.rust-lang.org/book/title-page.html)
--- rust-crash-course/target/debug/deps/rust_crash_course-473a58bf8fce4ace.d
+++
@@ -1,5 +0,0 @@
-/workspaces/learn-rust/rust-crash-course/target/debug/deps/rust_crash_course-473a58bf8fce4ace.rmeta: src/main.rs
-
-/workspaces/learn-rust/rust-crash-course/target/debug/deps/rust_crash_course-473a58bf8fce4ace.d: src/main.rs
-
-src/main.rs:
