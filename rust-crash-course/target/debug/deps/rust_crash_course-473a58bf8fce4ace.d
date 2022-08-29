7081d25ea7577988fb245dd5820eeb5d45b1b6cd
Tommy TIAN <xtianae@connect.ust.hk>
2022-08-29 00:50:32 +0000

    Initial commit.

---
+++ rust-crash-course/target/debug/deps/rust_crash_course-473a58bf8fce4ace.d
@@ -0,0 +1,5 @@
+/workspaces/learn-rust/rust-crash-course/target/debug/deps/rust_crash_course-473a58bf8fce4ace.rmeta: src/main.rs
+
+/workspaces/learn-rust/rust-crash-course/target/debug/deps/rust_crash_course-473a58bf8fce4ace.d: src/main.rs
+
+src/main.rs:
---
+++ rust-crash-course/target/debug/deps/rust_crash_course-953d4be1de6d74a7.d
@@ -0,0 +1,5 @@
+/workspaces/learn-rust/rust-crash-course/target/debug/deps/rust_crash_course-953d4be1de6d74a7.rmeta: src/main.rs
+
+/workspaces/learn-rust/rust-crash-course/target/debug/deps/rust_crash_course-953d4be1de6d74a7.d: src/main.rs
+
+src/main.rs:
