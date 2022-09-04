5a73aadae16c805e45bf5f2ba283667b7c48fc60
txaty <xtianae@connect.ust.hk>
2022-09-04 12:17:44 +0800

    4.2. References and Borrowing

---
+++ code/4_owernship/4_2_references_and_borrowing/references/target/debug/deps/references-dd86c193d6b5daad.d
@@ -0,0 +1,5 @@
+/Users/tommytian/codebase/learn-rust/code/4_owernship/4_2_references_and_borrowing/references/target/debug/deps/references-dd86c193d6b5daad: src/main.rs
+
+/Users/tommytian/codebase/learn-rust/code/4_owernship/4_2_references_and_borrowing/references/target/debug/deps/references-dd86c193d6b5daad.d: src/main.rs
+
+src/main.rs:
---
+++ code/4_owernship/4_2_references_and_borrowing/references/target/debug/references.d
@@ -0,0 +1,1 @@
+/Users/tommytian/codebase/learn-rust/code/4_owernship/4_2_references_and_borrowing/references/target/debug/references: /Users/tommytian/codebase/learn-rust/code/4_owernship/4_2_references_and_borrowing/references/src/main.rs
