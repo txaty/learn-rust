7a75da7f8624fca5f090d101325ec8151240bcdc
txaty <xtianae@connect.ust.hk>
2022-09-06 10:24:58 +0800

    5.3 Method syntax

---
+++ code/5_struct/5_3_method_syntax/associated_functions/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {}
