27e2abfcfb7e1a2f27ccabb3c10775e63b9dbf6f
txaty <txaty@proton.me>
2023-12-04 14:06:38 +0800

    2023-12-04 14:06:32: update notes.

---
+++ code/4_owernship/4_2_references_and_borrowing/implicit_dereference/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let x: Box<i32> = Box::new(-1);
+    let x_abs1 = i32::abs(*x); // explicit dereference
+    let x_abs2 = x.abs(); // implicit dereference
+    assert_eq!(x_abs1, x_abs2);
+
+    let r: &Box<i32> = &x;
+    let r_abs1 = i32::abs(**r); // explicit dereference (twice)
+    let r_abs2 = r.abs(); // implicit dereference (twice)
+    assert_eq!(r_abs1, r_abs2);
+
+    let s = String::from("Hello");
+    let s_len1 = str::len(&s); // explicit reference
+    let s_len2 = s.len(); // implicit reference
+    assert_eq!(s_len1, s_len2);
+}
